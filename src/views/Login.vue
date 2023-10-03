eb14478b7fed0f4fd1dd914a860350cddb6c18fe
Filip Tashkoski <filip.taskoski69@gmail.com>
2023-10-03 13:50:11 +0200

    fixed form error

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,57 +7,81 @@
     class="bg-cover bg-fixed bg-no-repeat min-h-screen"
   ></div>
   <div class="absolute inset-0 backdrop-blur-md">
-    <div class="flex flex-col items-center justify-center min-h-screen">
-      <form class="bg-white shadow-2xl rounded p-8 mb-4">
-        <h1 class="text-4xl font-bold mb-6 text-center">Login</h1>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-lg font-bold mb-2" for="email"
-            >Email</label
+    <div
+      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+    >
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
           >
-          <input
-            class="shadow appearance-none border rounded-xl w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-            type="text"
-            placeholder="Email"
-            v-model="email"
-          />
-        </div>
-        <div class="mb-6">
-          <label
-            class="block text-gray-700 text-lg font-bold mb-2"
-            for="password"
-            >Password</label
+            Sign in to your account
+          </h1>
+          <form
+            @submit.prevent="login"
+            class="space-y-4 md:space-y-6"
+            action="#"
           >
-          <input
-            class="shadow appearance-none border rounded-xl w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-            type="password"
-            placeholder="Password"
-            v-model="password"
-          />
-        </div>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Your email</label
+              >
+              <input
+                type="email"
+                v-model="email"
+                name="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter email"
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Password</label
+              >
+              <input
+                type="password"
+                v-model="password"
+                name="password"
+                id="password"
+                placeholder="Enter password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
 
-        <div class="mt-8 flex justify-center text-lg text-black">
-          <button
-            @click.prevent="login"
-            type="submit"
-            class="rounded-3xl bg-blue-500 hover:bg-blue-700 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300"
-          >
-            Login
-          </button>
+            <button
+              type="submit"
+              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Sign in
+            </button>
+            <p
+              v-if="errorMsg"
+              class="text-red-500 justify-center align-center flex"
+            >
+              {{ errorMsg }}
+            </p>
+            <p
+              class="text-sm font-light text-gray-500 justify-center align-center flex dark:text-gray-400"
+            >
+              Don’t have an account yet?&nbsp;
+              <a
+                href="#"
+                @click="redirectToRegister"
+                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Sign up
+              </a>
+            </p>
+          </form>
         </div>
-        <div class="mt-6">
-          <p class="text-gray-700 text-lg">
-            Don't have an account? Register now.
-          </p>
-          <button
-            class="text-blue-500 hover:underline focus:outline-none text-lg"
-            @click="redirectToRegister"
-          >
-            Register
-          </button>
-          <p v-if="errorMsg" class="text-red-500 justify-center align-center flex">{{ errorMsg }}</p>
-
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
@@ -72,18 +96,16 @@ const password = ref<string>("");
 const errorMsg = ref<string>("");
 
 const redirectToRegister = () => {
-  router.push("/register"); 
+  router.push("/register");
 };
 
 const login = async () => {
   const auth = getAuth();
   await signInWithEmailAndPassword(auth, email.value, password.value)
     .then(() => {
-      console.log("user loged");
       router.push("/");
     })
     .catch((error) => {
-      console.log("error", error.code);
 
       switch (error.code) {
         case "auth/invalid-email":
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,57 +7,81 @@
     class="bg-cover bg-fixed bg-no-repeat min-h-screen"
   ></div>
   <div class="absolute inset-0 backdrop-blur-md">
-    <div class="flex flex-col items-center justify-center min-h-screen">
-      <form class="bg-white shadow-2xl rounded p-8 mb-4">
-        <h1 class="text-4xl font-bold mb-6 text-center">Register</h1>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-lg font-bold mb-2" for="email"
-            >Email</label
+    <div
+      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+    >
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
           >
-          <input
-            class="shadow appearance-none border rounded-xl w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-            type="text"
-            placeholder="Email"
-            v-model="email"
-          />
-        </div>
-        <div class="mb-6">
-          <label
-            class="block text-gray-700 text-lg font-bold mb-2"
-            for="password"
-            >Password</label
+            Create an account
+          </h1>
+          <form
+            @submit.prevent="register"
+            class="space-y-4 md:space-y-6"
+            action="#"
           >
-          <input
-            class="shadow appearance-none border rounded-xl w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-            type="password"
-            placeholder="Password"
-            v-model="password"
-          />
-        </div>
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Your email</label
+              >
+              <input
+                type="email"
+                v-model="email"
+                name="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Enter email"
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Password</label
+              >
+              <input
+                type="password"
+                v-model="password"
+                name="password"
+                id="password"
+                placeholder="Enter password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
 
-        <div class="mt-8 flex justify-center text-lg text-black">
-          <button
-            @click.prevent="register"
-            type="submit"
-            class="rounded-3xl bg-blue-500 hover:bg-blue-700 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300"
-          >
-            Register
-          </button>
+            <button
+              type="submit"
+              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Sign up
+            </button>
+            <p
+              v-if="errorMsg"
+              class="text-red-500 justify-center align-center flex"
+            >
+              {{ errorMsg }}
+            </p>
+            <p
+              class="text-sm font-light text-gray-500 justify-center align-center flex dark:text-gray-400"
+            >
+              Already have an account?&nbsp;
+              <a
+                href="#"
+                @click="redirectToLogin"
+                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Log in
+              </a>
+            </p>
+          </form>
         </div>
-        <div class="mt-6">
-          <p class="text-gray-700 text-lg">
-            Already have an account? Login here
-          </p>
-          <button
-            class="text-blue-500 hover:underline focus:outline-none text-lg"
-            @click="redirectToLogin"
-          >
-            Login
-          </button>
-          <p v-if="errorMsg" class="text-red-500 justify-center align-center flex">{{ errorMsg }}</p>
-
-        </div>
-      </form>
+      </div>
     </div>
   </div>
 </template>
