3b0518bc68bf48157ccfdfc1005f166e3a41b353
ftaskoski123 <filip.taskoski69@gmail.com>
2023-10-05 00:03:19 +0200

    fixed the single move view

--- src/views/SingleMovie.vue
+++ src/views/SingleMovie.vue
@@ -1,39 +1,72 @@
 <template>
-    <div>
-      <div v-for="(movie, index) in movies" :key="movie.id || index ">
+  <div>
+    <button
+      @click="$router.go(-1)"
+      class="bg-red-500 px-4 py-2 rounded text-white font-bold hover:bg-red-700"
+    >
+      Back
+    </button>
+    <div v-for="movie in movies" :key="movie.id">
+      <div class="movie-info">
+      <div class="movie-img">
         <img
-          v-if="movie.backdrop_path"
-          :src="`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`"
+          :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
           alt=""
-          class="w-full h-auto"
         />
-        <p class="flex justify-center">{{ movie.original_title }}</p>
+      </div>
+      <div class="movie-content">
+        <h1>Title: {{ movie.title }}</h1>
+        <p class="movie-fact tagline">
+          <span>Tagline:</span> "{{ movie.tagline }}"
+        </p>
+        <p class="movie-fact">
+          <span>Released:</span>
+          {{
+            new Date(movie.release_date).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Duration:</span> {{ movie.runtime }} minutes
+        </p>
+        <p class="movie-fact">
+          <span>Revenue:</span>
+          {{
+            movie.revenue.toLocaleString('en-us', {
+              style: 'currency',
+              currency: 'USD',
+            })
+          }}
+        </p>
+        <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, onMounted } from 'vue';
-  import axios from 'axios';
-  import { useRoute } from 'vue-router';
-  
-  const route = useRoute();
-  const movies = ref<any>([]);
-  const apiKey = "7b5d0f8cf0589dd9221f592daa43db40";
-  
-  const getSingleMovie = async () => {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${route.params.movieId}?api_key=${apiKey}&language=en-US`
-    );
-    const data = await response;
-    console.log(data.data);
-  };
-  
-  onMounted(() => {
-    getSingleMovie();
-  });
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const movies = ref<any[]>([]);
+
+const apiKey = "7b5d0f8cf0589dd9221f592daa43db40";
+
+const getSingleMovie = async () => {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/${route.params.movieId}?api_key=${apiKey}&language=en-US`
+  );
+  const data = await response;
+  movies.value = [data.data];
+  console.log("movies", movies.value);
+};
+onMounted(() => {
+  getSingleMovie();
+});
+</script>
