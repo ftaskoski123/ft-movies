743246cee32d82399fa8fc2066aa4590d8468204
ftaskoski123 <filip.taskoski69@gmail.com>
2024-01-03 00:24:26 +0100

    phone style fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="relative">
-    <div>
+  <div class="relative min-h-screen">
+    <div class="absolute inset-0">
       <img
         src="../assets/movieHero.jpg"
         alt=""
@@ -9,7 +9,7 @@
     </div>
     <div class="absolute inset-0 backdrop-blur-md">
       <div
-        class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+        class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0"
       >
         <div
           class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
